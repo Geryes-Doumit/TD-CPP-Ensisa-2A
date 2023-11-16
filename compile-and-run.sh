04534e671b68f48f40e8bdb1b73f35918096c130
Geryes <geryes.doumet@gmail.com>
2023-11-16 20:10:56 +0100

    echo for readability

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -6,6 +6,7 @@ else # compile and run the files in the last directory
     path=$(ls -1 ./ | tail -n 1)
     path=$path/$(ls -1 $path | tail -n 1)/
     echo "Compiling *.cpp files in \"$path\"..."
+    echo
     
     g++ -o main.o $path/*.cpp
     if [ $? -eq 0 ]
