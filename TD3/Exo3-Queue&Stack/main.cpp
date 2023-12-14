a6986abfafae31fd7324f3c4589724cc7b950621
Geryes <geryes.doumet@gmail.com>
2023-12-14 15:43:50 +0100

    TD3 - Exo 3

--- TD3/Exo3-Queue&Stack/main.cpp
+++ TD3/Exo3-Queue&Stack/main.cpp
@@ -8,17 +8,16 @@ using namespace std;
 bool isPalindrome(const string& input){
     stack<char> s;
     queue<char> q;
-    for (int i = 0; i < input.size(); i++)
-    {
-        if(isalpha(input[i])){
+    for (int i = 0; i < input.size(); i++) {
+        if (isalpha(input[i])) {
             s.push(tolower(input[i]));
             q.push(tolower(input[i]));
         }
     }
-    while(!s.empty()){
-        if(s.top()!=q.front()){
+    while (!s.empty()) {
+        if(s.top()!=q.front()) 
             return false;
-        }
+
         s.pop();
         q.pop();
     }
